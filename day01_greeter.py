9a3feaee34c43d74c76d70a94886a9afc5e4763b
M.Rehman <rehmanch1510@gmail.com>
2026-06-23 02:28:47 -0700

    d1 2nd commit

--- day01_greeter.py
+++ day01_greeter.py
@@ -1,3 +1,17 @@
+#ans q1:string(str)sstore text reallife examlpe is name of person.
+#       integer(int) store whole number example: age of person
+#       float(float) store decimal number example: height of person
+#       boolean(true/false) store status example: is user is student?
+#ans q2:The print() function displays output on the screen.
+#      :The input() function asks the user for input and waits for them to type something.
+#      :Regardless of what the user types, input() always returns a string (str).
+#ans q3:A variable is a named container that stores data so you can use it later in your program.
+#      :rule for named a variable
+#      :R1.must start with letter or underscore
+#      :R2.can contain letter number and underscore
+#      :R3.hyphen are not allowed
+#      :R4.can be case sensitive
+#      :R5.no space, key words can not be a variable
 full_name = input("enter your full name please")
 age =int(input("enter your age"))
 internship_goal = input("what are your internship goals")
