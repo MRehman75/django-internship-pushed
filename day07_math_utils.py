efbe0abc36c1e84bd3d3aabd540e2d8a6e96271a
M.Rehman <rehmanch1510@gmail.com>
2026-06-29 02:53:03 -0700

    part A(math utiles) and Part B(student card generater) of day 07

---
+++ day07_math_utils.py
@@ -0,0 +1,108 @@
+#ans q25:
+#        A parameter and an argument are related, but they are used at different times.
+#        Parameter: A variable listed in a function's definition. It acts as a placeholder for the value the function will receive.
+#        Argument: The actual value you pass to the function when you call it.
+#ans q26:
+#        The return statement is used to send a value back from a function to the place where the function was called. It also ends the function immediately, 
+#        so any code after return is not executed.
+#        If a function does not have a return statement, Python automatically returns None.
+#ans q27:
+#        *args:Used to pass a variable number of positional arguments.Inside the function, it behaves like a tuple
+#       **kwargs: Used to pass a variable number of keyword arguments.Inside the function, it behaves like a dictionary
+#ans q28:
+#        Variable scope defines where a variable can be accessed in a program.
+#        A local variable is created inside a function and can only be used within that function.
+#       A global variable is created outside all functions and can be used anywhere in the program.
+#ans q29:
+#       Review of week one : day 02 was hardest because the operator and their calculation and testing the logic is working correctly other wise overall going perfectly
+#       and very intrestingly like guessing game and grade traker was very intresting. thanks to our senior software engineer to found me capable for this. thank you!
+import math
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(math.sqrt(n)) +1, 2):
+        if n % i == 0:
+            return False
+    return True
+def factorial(n):
+    if n < 0 :
+        return "invalid input"
+    result = 1
+    for i in range(1, n):
+        result *= i
+        return result
+def fibonacci(n):
+    if n <= 0:
+        return []
+    sequence =[0,1]
+    if n == 1:
+        return [0]
+    while len(sequence) < n:
+        sequence.append(sequence[-1] + sequence[-2])
+    return sequence
+    
+def is_palindrome(text):
+    cleaned= text.replace(" ","").lower()
+    return cleaned == cleaned[::-1]
+
+def celsius_to_fahrenheit(c):
+    return (c * 9/5) + 32
+
+def fahrenheit_to_celsius(f):
+    return (f-32) * 5/9
+
+def bmi_calculate(weight_kg,height_m):
+    bmi = weight_kg / (height_m ** 2)
+    if bmi < 18.5:
+        categore = "Under_weight"
+    elif bmi < 25:
+        categore = "Normal"
+    elif bmi < 30:
+        categore = "Over_weight"
+    else:
+        categore = "Obese"
+    return bmi, categore
+while True:
+    print("\n ================MATH UTILITIES MENU===================")
+    print("1. Prime check.")
+    print("2.Factorial ")
+    print("3. Fibonacci ")
+    print("4. Palindrome Check")
+    print("5. Celsius to Fahrenheit")
+    print("6. Fahrenheit to celsius")
+    print("7. BMI Calculator")
+    print("0. Exit")
+    
+    choice=input("enter your choice: ")
+    if choice =="1":
+       n =int( input("Enter Number: "))
+       print(" Prime : ", is_prime(n))
+    elif choice =="2":
+        n=int(input("Enter Number: "))
+        print("Factorial: ",factorial(n))
+    elif choice=="3":
+        n=int(input("Enter how many terms: "))
+        print("Fibonacci: ", fibonacci(n))
+    elif choice =="4":
+        n=input("Enter text: ")
+        print("Palindrome : ", is_palindrome(n))
+    elif choice == "5":
+        n= float(input("enter temp in celsius: "))
+        print("Celsius to Fahrenheit :",celsius_to_fahrenheit(n))
+    elif choice == "6":
+        n=float(input("enter temp in fahernheit: "))
+        print("Fahrenheit to celsius: ",fahrenheit_to_celsius(n))
+    elif choice == "7":
+        w=float(input("enter weight in kg"))
+        h=float(input("enter height in m"))
+        bmi,categore=bmi_calculate(w,h)
+        print(f" BMI: {bmi:2f}, Category: {categore}")
+    elif choice=="0":
+        print("Exit Pro...")
+        break
+    else:
+        print("invalid choice: try again")
---
+++ week1_project.py
@@ -0,0 +1,123 @@
+from datetime import datetime
+
+def calculate_average(grade):
+    return sum(grade.values()) /len(grade)
+
+def highest_subject(gardes):
+    subject= max(gardes, key=gardes.get)
+    return subject, gardes[subject]
+
+def lowest_subject(grades):
+    subject= min(grades, key=grades.get)
+    return subject, grades[subject]
+
+def letter_grade(average):
+    if average >= 90:
+        return "A"
+    elif average >= 80:
+        return "B"
+    elif average >= 70:
+        return "C"
+    elif average >= 60:
+        return "D"
+    else:
+        return "F"
+    
+def has_failed(grades):
+    return any(marks < 50 for marks in grades.values())
+
+def get_valid_grade(subject):
+    while True:
+        try:
+            grades =float(input(f"Enter marks for {subject}:"))
+            if 0 <= grades <= 100:
+                return grades
+            else:
+                print("MARKS should between 0 -100.")
+        except ValueError:
+            print("invalid input: please enter valid input.")
+
+def generate_report(student_name, grades):
+    avg = calculate_average(grades)
+    high_sub, high_marks = highest_subject(grades)
+    low_sub, low_marks = lowest_subject(grades)
+    grade = letter_grade(avg)
+    
+    report = []
+    report.append("=" * 50)
+    report.append(f"{'STUDENT REPORT CARD':^50}")
+    report.append("=" * 50)
+    report.append(f" Student Name : {student_name}")
+    report.append("-" * 50)
+    report.append(f"{'Subject':<25}{'Marks':>10}")
+    report.append("-" * 50)
+            
+    for subject, mark in grades.items():
+        report.append(f"{subject:<25}{mark:<10.2f}")
+    
+    report.append("-" * 50)
+    report.append(f"{'Average:':<25}{avg:>10.2f}")
+    report.append(f"{'Highest':<25}{high_sub}({high_marks:.2f})")
+    report.append(f"{'Lowest':<25}{low_sub} ({low_marks:.2f})")
+    report.append(f"{'Letter Grade':<25}{grade}")
+    report.append("=" * 50)
+    
+    return "\n" .join(report),avg
+students = []
+
+filename= f"report_{datetime.now().strftime('%Y-%m-%d')}.txt"
+
+with open(filename, "w") as file:
+    while True:
+        print("Enter Student Information")
+        print("-" * 30)
+        
+        student_name=input("Student Name:").strip()
+        
+        grades={}
+        
+        for i in range(1,6):
+            subject=input(f"Enter name of subject {i}").strip()
+            grades[subject] = get_valid_grade(subject)
+        report, average = generate_report(student_name, grades)
+        
+        print("\n")
+        print(report)
+        
+        file.write(report + "\n\n")
+        students.append({"name":student_name, "grades":grades,"average":average})
+        
+        choice = input("\nDo you want to enter another student? y/n").strip().lower()
+        if choice !="y":
+            break
+
+print("\n")
+print("=" * 60)
+print(f"{'CLASS SUMMARY':^60}")
+print("="* 60)
+
+if students:
+    class_average=sum(student["average"] for student in students) /len(students)
+    top_students = max(students, key=lambda s: s["average"])
+    
+    failed_students =[student["name"] for student in students if has_failed(student["grades"])]
+    
+    print(f"Number of Students: {len(students)}")
+    print(f"Class Average     : {class_average:.2f}")
+    print(f"Top Students      : {top_students["name"]}({top_students['average']:.2f})")
+    
+    print("\n Students who Failed (any subject below 50): ")
+    
+    if failed_students:
+        for name in failed_students:
+            print(f"_{name}")
+    else:
+        print("None")
+        
+print("=" * 60)
+print(f"\nReport Card EXported to:{filename}")
+
+    
+
+
+        
